1e8481d7eca58df2dabcff378e68d016b0ac3b9d
luizfernandoppjr <luizfernandoppjr@gmail.com>
2019-09-07 00:12:16 -0300

    gerador de texto de renda mensal

--- Trabalho 1_2 LDP2019_2.py
+++ Trabalho 1_2 LDP2019_2.py
@@ -15,9 +15,28 @@ class Populacao:
     def __init__(self, tamanho):
         self.tamanho = tamanho
 
+        #rendimento mensal domiciliar per capita
         self.rendimento_mensal_domiciliar_valores = [472, 1442, 2551, 1176, 745, 496, 360]
+        self.rendimento_mensal_domiciliar_texto = ["Sem rendimento a 1/2 salário mínimo",
+                                                    "Mais de 1/2 a 1 salário mínimo",
+                                                    "Mais de 1 a 2 salários mínimos",
+                                                    "Mais de 2 a 3 salários mínimos",
+                                                    "Mais de 3 a 5 salários mínimos",
+                                                    "Mais de 5 salários mínimos",
+                                                    "Sem declaração"]
         self.rendimento_mensal_domiciliar_media = sum(self.rendimento_mensal_domiciliar_valores) / len(self.rendimento_mensal_domiciliar_valores)
         self.rendimento_mensal_domiciliar_distrib = [x / sum(self.rendimento_mensal_domiciliar_valores) for x in self.rendimento_mensal_domiciliar_valores]
+
+
+        gerador = np.random.multinomial(100, self.rendimento_mensal_domiciliar_distrib)
+        
+        for i in range(len(self.rendimento_mensal_domiciliar_distrib)):
+            if gerador <= sum(self.rendimento_mensal_domiciliar_distrib[0:i]):
+                renda_individuo = i
+                break
+
+        
+
 
         self.individuos = [Pessoa(np.random.multinomial(self.tamanho, self.rendimento_mensal_domiciliar_distrib) * self.rendimento_mensal_domiciliar_media)  for i in range(tamanho)]
 
