94d5b26a317ae473eedca7eeaac22916cb7d643f
yagofalcao <42628080+yagofalcao@users.noreply.github.com>
2019-09-07 03:36:39 -0300

    Todas as Classes (Corrigido)

--- Trabalho 1_2 LDP2019_2.py
+++ Trabalho 1_2 LDP2019_2.py
@@ -2,14 +2,14 @@ import numpy as np
 import random as rnd
 
 class Pessoa:
-    def __init__(self, rendimento_mensal_domiciliar, rede_ensino, modalidade,sexo,cor):#regiao, idade):
+    def __init__(self, rendimento_mensal_domiciliar, rede_ensino, modalidade,sexo,cor,regiao,idade):
         self.rendimento_mensal_domiciliar = rendimento_mensal_domiciliar
         self.rede_ensino = rede_ensino
         self.modalidade = modalidade
         self.sexo = sexo
         self.cor = cor
-        #self.regiao = regiao
-        #self.idade = idade
+        self.regiao = regiao
+        self.idade = idade
 
 class Populacao:
     def __init__(self, tamanho):
