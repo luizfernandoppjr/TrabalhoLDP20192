84d89a70e8007abd787634929db60d4cdbc43880
luizfernandoppjr <luizfernandoppjr@gmail.com>
2019-09-06 23:29:10 -0300

    Transformação das classes em arquivos separados

---
+++ Pessoa.py
@@ -0,0 +1,9 @@
+class Pessoa:
+    def __init__(self, rendimento_mensal_domiciliar):#, rede_ensino, modalidade, sexo, cor, regiao, idade):
+        self.rendimento_mensal_domiciliar = rendimento_mensal_domiciliar
+        #self.rede_ensino = rede_ensino
+        #self.modalidade = modalidade
+        #self.sexo = sexo
+        #self.cor = cor
+        #self.regiao = regiao
+        #self.idade = idade
