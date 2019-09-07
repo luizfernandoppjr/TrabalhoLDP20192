d4abb08092afe43085123c305e6a7a26520d07c4
luizfernandoppjr <luizfernandoppjr@gmail.com>
2019-09-07 01:03:03 -0300

    Comentários para deixar mais didático o código

--- Trabalho 1_2 LDP2019_2.py
+++ Trabalho 1_2 LDP2019_2.py
@@ -15,7 +15,9 @@ class Populacao:
     def __init__(self, tamanho):
         self.tamanho = tamanho
 
-        #rendimento mensal domiciliar per capita
+        #rendimento mensal domiciliar per capita//
+
+        #Estabelece os valores e distribuição dessa categoria pelos dados da PNAD
         self.rendimento_mensal_domiciliar_valores = [472.0, 1442.0, 2551.0, 1176.0, 745.0, 496.0, 360.0]
         self.rendimento_mensal_domiciliar_texto = ["Sem rendimento a 1/2 salário mínimo",
                                                     "Mais de 1/2 a 1 salário mínimo",
@@ -27,15 +29,21 @@ class Populacao:
         self.rendimento_mensal_domiciliar_media = sum(self.rendimento_mensal_domiciliar_valores) / len(self.rendimento_mensal_domiciliar_valores)
         self.rendimento_mensal_domiciliar_distrib = [x / sum(self.rendimento_mensal_domiciliar_valores) for x in self.rendimento_mensal_domiciliar_valores]
 
+        #Gera uma população numérica tomando a ditribuição dos valores da PNAD
         gerador = list(np.random.multinomial(self.tamanho, self.rendimento_mensal_domiciliar_distrib))
         
+        #Categoriza essa população: de número para a categoria em texto
         renda_individuo = [0] * self.tamanho
         k = 0
         for j in range(len(gerador)):
             for i in range(gerador[j]):
                 renda_individuo[k] = self.rendimento_mensal_domiciliar_texto[j]
                 k += 1
-
+        
+        #//rendimento mensal domiciliar per capita
+        
+        
+        #Gera a população
         self.individuos = [Pessoa(renda_individuo[x]) for x in range(len(renda_individuo))]
 
 
@@ -43,5 +51,5 @@ class Populacao:
         return rnd.sample(self.individuos, n)
 
 
-pop = Populacao(3)
+pop = Populacao(10)
 print(pop.individuos[1].rendimento_mensal_domiciliar)
